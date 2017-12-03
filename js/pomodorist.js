ad2c20b1681afcb9f16f6f0f210ab7fc8842c794
vvll99 <10vladan@gmail.com>
2017-12-03 13:27:08 +0100

    final

--- js/pomodorist.js
+++ js/pomodorist.js
@@ -1,45 +1,41 @@
-/*var initialTime = Date.now();
 
-function checkTime(){
-  var timeDifference = Date.now() - initialTime;
-  var formatted = convertTime(timeDifference);
-  document.getElementById('time').innerHTML = '' + formatted;
-}
-
-function convertTime(miliseconds) {
-  var totalSeconds = Math.floor(miliseconds/1000);
-  var minutes = Math.floor(totalSeconds/60);
-  var seconds = totalSeconds - minutes * 60;
-  return minutes + ':' + seconds;
-}
-window.setInterval(checkTime, 100);*/
 
 var minutes = 25;
-var seconds = "00";
+var seconds = 0;
 
 function countdown() {
 
 
-    if (seconds <= 0) {
+    if (seconds < 0) {
         minutes--;
         seconds = 59;
     }
-    if (minutes <= 0) {
-        minutes = 24;
-        seconds = 59;
+    if (minutes < 0) {
+        minutes = 25;
+        seconds = 0;
     }
 
-    document.getElementById("time").innerHTML = minutes + ':' + seconds;
+    document.getElementById("time").innerHTML = pad(minutes) + ':' + pad(seconds);
     seconds--;
 }
-//var interval = window.setInterval(countdown, 1000);
+
+function pad(number)
+{
+    if ( number < 10)
+    {
+        return ( '0' + number);
+    }
+    else {
+        return number;
+    }
+}
+
 var interval;
 
 function start() {
     clearInterval(interval);
 
-    //console.log("started ..")
-    //window.setInterval(countdown, 1000);
+    
     interval = window.setInterval(countdown, 1000);
 
 
@@ -52,9 +48,14 @@ function stop() {
 
 }
 
-document.getElementById("time").innerHTML = minutes + ':' + seconds;
-seconds--;
+document.getElementById("time").innerHTML = minutes + ':' + pad(seconds);
+
 
 function reset() {
-
+        minutes = 25;
+        seconds = 0;
+    stop();
+    document.getElementById("time").innerHTML = minutes + ':' + pad(seconds);
+    
 }
+
